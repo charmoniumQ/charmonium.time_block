ee42e50f1e78aff8bc931a907fee42dc93dad53e
Samuel Grayson <sam@samgrayson.me>
2024-02-15 01:06:36 -0600

    Bump version: 0.3.3 → 0.3.4

--- charmonium/time_block/__init__.py
+++ charmonium/time_block/__init__.py
@@ -7,7 +7,7 @@ from .time_block import TimeBlock, logger
 
 __author__ = "Samuel Grayson"
 __email__ = "sam+dev@samgrayson.me"
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 __license__ = "MPL-2.0"
 __copyright__ = "2020, Samuel Grayson"
 
